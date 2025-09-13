68af513be5b458cb321df099b0209754909e0b62
Ghaida Ja'aysah <ghaida.20004@gmail.com>
2025-09-13 23:17:47 +0300

    get_input_args.py DONE

--- get_input_args.py
+++ get_input_args.py
@@ -44,8 +44,9 @@ def get_input_args():
     #Create parse object
     parser = argparse.ArgumentParser(description="Retrieve command line arguments for CNN image classifier")
     
+    # create 3 command line arguments
     parser.add_argument('--dir', type=str, default='pet_images/', help='path to the folder of pet images')
     parser.add_argument('--arch', type=str, default='vgg', help='CNN model architecture to use')
     parser.add_argument('--dogfile', type=str, default='dognames.txt', help='text file that contains dognames')
-    
+  
     return parser.parse_args()
